1f11f20172dba48c89483c772339915429f25f52
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-10-30 09:36:50 +0100

    Add project files.

---
+++ Data/ServiceContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.Reflection.Emit;
+using Data;
+
+namespace Data
+{
+    public class ServiceContext : DbContext
+    {
+        public ServiceContext(DbContextOptions<ServiceContext> options) : base(options) { }
+        public DbSet <ImageItem> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ImageItem>(entity =>
+            {
+                entity.ToTable("Images");
+                //entity.HasKey(i => i.IdImage);
+            });
+
+
+
+        }
+
+
+
+    }
+}
+public class ServiceContextFactory : IDesignTimeDbContextFactory<ServiceContext>
+{
+    public ServiceContext CreateDbContext(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", false, true);
+        var config = builder.Build();
+        var connectionString = config.GetConnectionString("ServiceContext");
+        var optionsBuilder = new DbContextOptionsBuilder<ServiceContext>();
+        optionsBuilder.UseSqlServer(config.GetConnectionString("ServiceContext"));
+
+        return new ServiceContext(optionsBuilder.Options);
+    }
+}
