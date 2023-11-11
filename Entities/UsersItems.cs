5855c322b23cf0ce055f18562940b30f48599a19
isabellozano37 <isabeloficial37@gmail.com>
2023-11-11 12:44:39 +0100

    Users

--- Entities/UsersItems.cs
+++ Entities/UsersItems.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class UsersItems
+    public class UsersItems
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Users { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
