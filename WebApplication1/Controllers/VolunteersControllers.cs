7ccfc64e8f1b4ebc2e736a35e2da2c44971adc12
isabellozano37 <isabeloficial37@gmail.com>
2023-11-13 17:11:41 +0100

    Voluntarios

--- WebApplication1/Controllers/VolunteersControllers.cs
+++ WebApplication1/Controllers/VolunteersControllers.cs
@@ -35,6 +35,56 @@ namespace WebApplicationParkinson.Controllers
                 return StatusCode(500, $"Error al obtener el ID del rol: {ex.Message}");
             }
         }
+
+        [HttpGet(Name = "GetVolunteers")]
+        public IActionResult GetVolunteers()
+        {
+            var Volunteers = _serviceContext.Volunteers.ToList();
+            return Ok(Volunteers);
+
+        }
+
+        [HttpDelete(Name = "DeleteVolunteers")]
+        public IActionResult DeleteVolunteers(string Name)
+        {
+            var Volunteers = _serviceContext.Volunteers.FirstOrDefault(p => p.Name == Name);
+            if (Volunteers != null)
+            {
+                _serviceContext.Volunteers.Remove(Volunteers);
+                _serviceContext.SaveChanges();
+                return Ok("La cancion se ha eliminado correctamente.");
+            }
+            else
+            {
+                return NotFound("no se ha encontrado la cancion con el identificador especificado.");
+            }
+        }
+
+        [HttpPut(Name = "UpdateVolunteers")]
+        public IActionResult UpdateVolunteers(string Name, [FromBody] VolunteersItems updatedVolunteers)
+        {
+            var Volunteers = _serviceContext.Volunteers.FirstOrDefault(p => p.Name == Name);
+            if (Volunteers != null)
+            {
+                Volunteers.Name = updatedVolunteers.Name;
+                Volunteers.Dni = updatedVolunteers.Dni;
+                Volunteers.Birthdate = updatedVolunteers.Birthdate;
+                Volunteers.Address = updatedVolunteers.Address;
+                Volunteers.Phone = updatedVolunteers.Phone;
+                Volunteers.Email = updatedVolunteers.Email;
+                Volunteers.Education = updatedVolunteers.Education;
+                Volunteers.Id_Shift = updatedVolunteers.Id_Shift;
+                Volunteers.TermsAccepted = updatedVolunteers.TermsAccepted;
+
+                _serviceContext.SaveChanges();
+                return Ok("La cancion se ha actualizado correctamente.");
+            }
+            else
+            {
+                return NotFound("No se ha encontrado la cancion con el identificador especificado.");
+            }
+        }
+
     }
-    
+
 }
